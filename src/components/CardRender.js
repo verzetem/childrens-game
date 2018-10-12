a45d867b5af9852f8c64a2578d75e907d635390a
chris <verzetem@gmail.com>
2018-10-12 11:15:54 -0600

    added images in a row, randomized images on gameboard

--- src/components/CardRender.js
+++ src/components/CardRender.js
@@ -3,16 +3,34 @@ import React from 'react';
 
 const CardRender = ({img}) => {
 
+let cards = img.map((image, i) => {
+	for (var prop in image) {
+		return <div><img id={prop} className="card" src={image[prop]} /></div>
+		}
+})
+
+let newArr = []
+	 for (let i = 0; i < cards.length; i++) {
+	 	newArr.push(cards[i])
+	 	newArr.push(cards[i])
+	 	console.log(newArr)
+	 }
+
+const shuffleArray = (array) => {
+	for (var i = array.length - 1; i > 0; i--) {
+	  var j = Math.floor(Math.random() * (i + 1));
+	  var temp = array[i];
+	  array[i] = array[j];
+	  array[j] = temp;
+	 } 
+		return (array)
+	}
+
 	return (
 
-		<div>
-			{img.map((image, i) => {
-				// return <img src={image[i]} />
-				for (var prop in image) {
-					return <img src={image[prop]} />
-				}
-			})}
-		</div>
+		<>
+			{shuffleArray(newArr)}
+		</>
 	)
 }
 
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -6,7 +6,11 @@ const GameBoard = ({img}) => {
 	return (
 		<div>
 			<h1>Game Board</h1>
-			<CardRender img={img} />
+			<div className="gameBoard">
+				
+						<CardRender img={img} />
+					
+			</div>
 		</div>
 	)
 }
